a4651730919d22fc8f064caab6a4550b29f0611b
valeromero <valeriagromero@hotmail.com>
2022-09-20 19:21:28 -0300

    DOM

---
+++ main.js
@@ -0,0 +1,66 @@
+const enlace = document.createElement('A');
+
+enlace.textContent = 'Carrito';
+
+console.log(enlace);
+
+enlace.href = '/carrito';
+
+console.log(enlace);
+
+
+
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(enlace);
+
+enlace.onclick = miCarrito;
+function miCarrito(){
+    alert("Cargaste al carrito");
+}
+
+const texto1 = document.createElement('P');
+texto1.textContent = 'actas';
+texto1.classList.add('parroquiales', 'actas');
+
+const texto2 = document.createElement('P');
+texto2.textContent = 'Parroquiales';
+texto2.classList.add('titulo');
+
+const texto3 = document.createElement('P');
+texto3.textContent = '€ 190 por persona';
+texto3.classList.add('precio');
+
+
+console.log(texto1);
+console.log(texto2);
+console.log(texto3);
+
+const info = document.createElement('div');
+info.classList.add('info');
+info.appendChild(texto1)
+info.appendChild(texto2)
+info.appendChild(texto3)
+
+console.log(info);
+
+const imagen = document.createElement('img');
+imagen.src = './asset/imagenes/laptop.svg';
+
+const card = document.createElement('div');
+card.classList.add('card');
+card.appendChild(imagen);
+card.appendChild(info);
+
+console.log(card);
+
+const contenedor = document.querySelector('.hacer .contenedor-cards');
+contenedor.appendChild(card);
+
+
+
+
+
+
+
+
+
